76238cf7f67bbf4c41df0a3dc1b40087d9351e84
AlexBarsky <bogomolov2004alex@gmail.com>
2024-02-27 23:55:57 -0800

    findViewById

--- Lesson1/control_lesson1/src/main/java/ru/mirea/bogomolovaa/control_lesson1/MainActivity.kt
+++ Lesson1/control_lesson1/src/main/java/ru/mirea/bogomolovaa/control_lesson1/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.mirea.bogomolovaa.control_lesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_main)
+
+        val myTextView: TextView = findViewById(R.id.textView)
+        myTextView.text = "New text in MIREA"
+
+        val button: Button = findViewById(R.id.main_button)
+        button.text = "MireaButton"
+
+        val checkBox: CheckBox = findViewById(R.id.checkBox)
+        checkBox.isChecked = true
     }
 }
